bd6d949ed2ee58fa9338cb97f931c8607e591e3e
Meet Bhandari <159768942+Bhandari1309@users.noreply.github.com>
2024-02-22 13:54:23 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,7 +1,18 @@
 class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+        def romanToInt(self, s: str) -> int:
+            roman_map = {
+                'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+            }
+            
+            result = 0
+            last_value = 0
+            
+            for char in s[::-1]:
+                value = roman_map[char]
+                if value < last_value:
+                    result -= value
+                else:
+                    result += value
+                    last_value = value
+            
+            return result
